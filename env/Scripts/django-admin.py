c8b6eebde250ca740818fe50455af8afee94b9d0
Alfredo Rafael Pérez Arroyo <ARPEREZ@phergal.com>
2021-05-04 18:52:15 +0200

    primer commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\arperez.phergal\downloads\crm-phergal\django-star-admin\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
